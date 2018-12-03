6d843a92d5b9c08439b89b58531d7b7b9c4a4313
Kanwal19 <44018737+Kanwal19@users.noreply.github.com>
2018-12-03 15:34:50 +0530

    Update CapBookApplication.java

--- CapBook/src/main/java/com/cg/capbook/CapBookApplication.java
+++ CapBook/src/main/java/com/cg/capbook/CapBookApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CapBookApplication {
-
+//Kanwal added this
 	public static void main(String[] args) {
 		SpringApplication.run(CapBookApplication.class, args);
 	}
